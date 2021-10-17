25827d7b84cba18ea0d9066d1dd453053b80fbdf
Ryan Wilson <ryan.wilson@onlinehomeretail.co.uk>
2021-10-17 22:16:33 +0100

    write first test case

--- test_app.py
+++ test_app.py
@@ -3,3 +3,36 @@ from flask_sqlalchemy import SQLAlchemy
 
 from app import create_app
 from models import setup_db, Actor, Movie, MovieRole
+
+class CastingAgencyTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app()
+        self.client = self.app.test_client
+        self.database_name = "casting_agency_test"
+        self.database_path = "postgresql://{}/{}".format('localhost:5432', self.database_name)
+        setup_db(self.app, self.database_path)
+
+        with self.app.app_context():
+            self.db = SQLAlchemy()
+            self.db.init_app(self.app)
+            self.db.create_all()
+
+        self.new_actor = {
+            'name': 'Ryan Reynolds',
+            'age': 44,
+            'gender': 'Male'
+        }
+
+    def tearDown(self):
+        pass
+
+    def test_get_actors(self):
+        response = self.client().get('/actors')
+        data = json.loads(response.data)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertTrue(len(data['actors']))
+
+if __name__ == "__main__":
+    unittest.main()
