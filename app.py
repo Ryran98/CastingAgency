5ffa59ee74f0e3b42e555dc282c534db95a46760
Ryan Wilson <ryan.wilson@onlinehomeretail.co.uk>
2021-10-17 21:38:07 +0100

    setup all crud endpoints for actors and movies

--- app.py
+++ app.py
@@ -2,11 +2,289 @@ import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from models import db, Actor, Movie, MovieRole, setup_db
+from auth.auth import AuthError, requires_auth
 
 def create_app(test_config=None):
   # create and configure the app
   app = Flask(__name__)
-  CORS(app)
+  setup_db(app)
+
+  cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+  @app.after_request
+  def after_request(response):
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PATCH,POST,DELETE,OPTIONS')
+    return response
+
+  @app.route('/actors', methods=['GET'])
+  def get_actors():
+    actors = Actor.query.all()
+
+    if not actors or len(actors) < 1:
+      abort(404)
+    
+    try:
+      actors_formatted = []
+      for actor in actors:
+        actors_formatted.append(actor.format())
+
+      return jsonify({
+        'success': True,
+        'actors': actors_formatted
+      }), 200
+
+    except:
+      abort(422)
+
+  @app.route('/movies', methods=['GET'])
+  def get_movies():
+    movies = Movie.query.all()
+
+    if not movies or len(movies) < 1:
+      abort(404)
+    
+    try:
+      movies_formatted = []
+      for movie in movies:
+        movies_formatted.append(movie.format())
+      
+      return jsonify({
+        'success': True,
+        'movies': movies_formatted
+      }), 200
+
+    except:
+      abort(422)
+
+  @app.route('/actors/<int:actor_id>', methods=['DELETE'])
+  def delete_actor(actor_id):
+    actor = Actor.query.get(actor_id)
+
+    if actor is None:
+      abort(404)
+
+    try:
+      actor.delete()
+
+      return jsonify({
+        'success': True,
+        'id': actor_id
+      }), 200
+
+    except:
+      abort(422)
+
+  @app.route('/movies/<int:movie_id>', methods=['DELETE'])
+  def delete_movie(movie_id):
+    movie = Movie.query.get(movie_id)
+
+    if movie is None:
+      abort(404)
+    
+    try:
+      movie.delete()
+
+      return jsonify({
+        'success': True,
+        'id': movie_id
+      }), 200
+
+    except:
+      abort(422)
+
+  @app.route('/actors', methods=['POST'])
+  def new_actor():
+    body = request.get_json()
+    name = body.get('name', None)
+    age = body.get('age', None)
+    gender = body.get('gender', None)
+
+    if not name:
+      return jsonify({
+        'success': False,
+        "error": 422,
+        'message': 'Missing name'
+      })
+    if not age:
+      return jsonify({
+        'success': False,
+        "error": 422,
+        'message': 'Missing age'
+      })
+    if not gender:
+      return jsonify({
+        'success': False,
+        "error": 422,
+        'message': 'Missing gender'
+      })
+      
+    try:
+      actor = Actor(name=name, age=age, gender=gender)
+      actor.insert()
+
+      return jsonify({
+        'success': True,
+        'id': actor.id
+      }), 200
+
+    except:
+      abort(422)
+
+  @app.route('/movies', methods=['POST'])
+  def new_movie():
+    body = request.get_json()
+    title = body.get('title', None)
+    release_date = body.get('release_date', None)
+
+    if not title:
+      return jsonify({
+        'success': False,
+        "error": 422,
+        'message': 'Missing title'
+      })
+    if not release_date:
+      return jsonify({
+        'success': False,
+        "error": 422,
+        'message': 'Missing release date'
+      })
+
+    try:
+      movie = Movie(title=title, release_date=release_date)
+      movie.insert()
+
+      return jsonify({
+        'success': True,
+        'id': movie.id
+      }), 200
+
+    except:
+      abort(422)
+
+  @app.route('/actors/<int:actor_id>', methods=['PATCH'])
+  def update_actor(actor_id):
+    actor = Actor.query.get(actor_id)
+
+    if not actor:
+      abort(404)
+
+    body = request.get_json()
+    name = body.get('name', None)
+    age = body.get('age', None)
+    gender = body.get('gender', None)
+
+    try:
+      if name is not None:
+        actor.name = name
+      if age is not None:
+        actor.age = age
+      if gender is not None:
+        actor.gender = gender
+
+      actor.update()
+
+      return jsonify({
+        'success': True,
+        'actor': actor.format()
+      }), 200
+
+    except:
+      abort(422)
+
+  @app.route('/movies/<int:movie_id>', methods=['PATCH'])
+  def update_movie(movie_id):
+    movie = Movie.query.get(movie_id)
+
+    if not movie:
+      abort(404)
+
+    body = request.get_json()
+    title = body.get('title', None)
+    release_date = body.get('release_date', None)
+
+    try:
+      if title is not None:
+        movie.title = title
+      if release_date is not None:
+        movie.release_date = release_date
+
+      movie.update()
+
+      return jsonify({
+        'success': True,
+        'movie': movie.format()
+      }), 200
+    
+    except:
+      abort(422)
+
+  @app.route('/movieroles', methods=['POST'])
+  def new_movie_role():
+    body = request.get_json()
+    actor_id = body.get('actor_id', None)
+    movie_id = body.get('movie_id', None)
+
+    actor = Actor.query.get(actor_id)
+    movie = Movie.query.get(movie_id)
+
+    if not actor:
+      return jsonify({
+        'success': False,
+        'error': 422,
+        'message': 'No actor could be found for id ' + actor_id
+      })
+    if not movie:
+      return jsonify({
+        'success': False,
+        'error': 422,
+        'message': 'No movie could be found for id ' + movie_id
+      })
+
+    try:
+      movie_role = MovieRole(actor=actor, movie=movie)
+      movie_role.insert()
+
+      return jsonify({
+        'success': True,
+        'id': movie_role.id
+      })
+
+    except:
+      abort(422)
+
+  @app.errorhandler(400)
+  def bad_request(error):
+    return jsonify({
+      'success': False,
+      'error': 400,
+      'message': 'bad request'
+    }), 400
+
+  @app.errorhandler(404)
+  def not_found(error):
+    return jsonify({
+      'success': False,
+      'error': 404,
+      'message': 'resource not found'
+    }), 404
+
+  @app.errorhandler(405)
+  def not_allowed(error):
+    return jsonify({
+      'success': False,
+      'error': 405,
+      'message': 'method not allowed'
+    }), 405
+
+  @app.errorhandler(422)
+  def unprocessable(error):
+    return jsonify({
+      'success': False,
+      'error': 422,
+      'message': 'unprocessable'
+    }), 422
 
   return app
 
---
+++ models.py
@@ -0,0 +1,109 @@
+from flask_sqlalchemy import SQLAlchemy
+import sys
+
+database_name = "casting_agency"
+database_path = "postgresql://{}/{}".format('localhost:5432', database_name)
+
+db = SQLAlchemy()
+
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.drop_all()
+    #db.create_all()
+
+class Actor(db.Model):
+    __tablename__ = 'Actor'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    age = db.Column(db.Integer)
+    gender = db.Column(db.String)
+    movie_roles = db.relationship('MovieRole', backref='actor', lazy=True, cascade='all, delete')
+
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+    
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'age': self.age,
+            'gender': self.gender,
+            'movies': [{
+                'id': role.movie.id,
+                'title': role.movie.title,
+                'release_date': role.movie.release_date.strftime("%d-%m-%Y")
+            } for role in self.movie_roles]
+        }
+
+class Movie(db.Model):
+    __tablename__ = 'Movie'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    release_date = db.Column(db.DateTime, nullable=False)
+    movie_roles = db.relationship('MovieRole', backref='movie', lazy=True, cascade='all, delete')
+
+    def __init__(self, title, release_date):
+        self.title = title
+        self.release_date = release_date
+    
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'release_date': self.release_date.strftime("%d-%m-%Y"),
+            'actors': [{
+                'id': role.actor.id,
+                'name': role.actor.name
+            } for role in self.movie_roles]
+        }
+
+class MovieRole(db.Model):
+    __tablename__ = 'MovieRole'
+
+    id = db.Column(db.Integer, primary_key=True)
+    actor_id = db.Column(db.Integer, db.ForeignKey('Actor.id'), nullable=False)
+    movie_id = db.Column(db.Integer, db.ForeignKey('Movie.id'), nullable=False)
+
+    def __init__(self, actor, movie):
+        self.actor = actor
+        self.movie = movie
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
