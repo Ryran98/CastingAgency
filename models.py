d994f8f6f157b3138027fffa422dd8ae04c2311b
Ryan Wilson <ryan.wilson@onlinehomeretail.co.uk>
2021-10-19 14:09:57 +0100

    remove drop and create all from setupdb method

--- models.py
+++ models.py
@@ -11,8 +11,8 @@ def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
-    db.drop_all()
-    db.create_all()
+    #db.drop_all()
+    #db.create_all()
 
 class Actor(db.Model):
     __tablename__ = 'Actor'
